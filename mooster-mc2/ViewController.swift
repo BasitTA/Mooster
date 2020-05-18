c4c34aa26183e5bbf75379cec4930bdcfc3d5345
Basit <bta.aang@gmail.com>
2020-05-18 15:15:20 +0700

    Initial Commit

---
+++ mooster-mc2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  mooster-mc2
+//
+//  Created by Basit Tri Anggoro on 18/05/20.
+//  Copyright © 2020 Basit Tri Anggoro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
